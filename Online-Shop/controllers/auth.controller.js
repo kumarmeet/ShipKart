0e85b10dc3781d8a78c9e0b71cdbb28d0ed92ef7
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-11-24 20:08:45 +0530

    V0.1

---
+++ Online-Shop/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const getSignUp = (req, res) => {
+  res.render("");
+};
+
+const getLogin = (req, res) => {
+  res.render("");
+};
+
+module.exports = {
+  getSignUp: getSignUp,
+  getLogin: getLogin,
+};
